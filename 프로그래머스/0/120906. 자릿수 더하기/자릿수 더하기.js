0891c294d19c4079cb7b53727cc8efc668fd4b6d
ahreum <117738875+aahreum@users.noreply.github.com>
2025-08-13 16:47:37 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.js
@@ -1,8 +1,20 @@
 function solution(n) {
     let answer = 0;
-    let string = String(n);
-    for (let i = 0; i < string.length; i++) {  
-        answer += +string[i];
+    let str = String(n);
+    for (let i = 0; i < str.length; i++) {  
+        answer += +str[i];
     }
     return answer;
+}
+
+// 형 변환 없이 숫자 그대로 푸는 방법
+function solution(n) {
+    let result = 0;
+    
+    while (n > 0) {
+        result += n % 10;
+        n = Math.floor(n/10);
+    }
+
+    return result;
 }
