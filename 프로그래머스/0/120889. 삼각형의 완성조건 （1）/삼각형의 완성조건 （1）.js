f6a30bb032e9724859c5f759d0f99a4bf9573f82
ahreum <117738875+aahreum@users.noreply.github.com>
2025-08-18 16:19:00 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.js
@@ -1,8 +1,14 @@
 function solution(sides) {
-    let sidesArray = sides.sort((a, b) => a - b);
-    if (sidesArray[2] < sidesArray[0] + sidesArray[1]) {
+    let sortSides = sides.sort((a, b) => a - b);
+    if (sortSides[2] < sortSides[0] + sortSides[1]) {
         return 1;
     } else {
         return 2;
     }
+}
+
+// 구조 분해 할당을 사용한 방법
+function solution(sides) {
+    const [long, a, b] = sides.sort((a,b) => b-a);
+    return long < a + b ? 1 : 2
 }
