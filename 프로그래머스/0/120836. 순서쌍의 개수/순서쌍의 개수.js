53242f265eb188218d4235ff22566ced3eb018b7
ahreum <117738875+aahreum@users.noreply.github.com>
2025-08-23 12:42:54 +0900

    [level 0] Title: 순서쌍의 개수, Time: 30.05 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -1,16 +1,6 @@
 function solution(n) {
     let answer = 0;
-    
-    // 풀이 1
-    // for (let i = 1; i <= n; i++) {
-    //     for (let j = 1; j <= n; j++) {
-    //         if (i * j === n) {
-    //             answer++;
-    //         }
-    //     }
-    // }
-    
-    //풀이 2
+
     for (let i = 1; i <= n; i++) {
         if (n % i === 0) {
             answer++;
