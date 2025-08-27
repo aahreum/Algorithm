4beb24aee5b654c4610091b92b09dbeac7cc27bc
ahreum <117738875+aahreum@users.noreply.github.com>
2025-08-27 15:02:53 +0900

    [Bronze V] Title: 나머지, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.js
@@ -0,0 +1,15 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split(' ')
+
+function solution(input) {
+  const a = Number(input[0])
+  const b = Number(input[1])
+  const c = Number(input[2])
+
+  console.log((a + b) % c)
+  console.log(((a % c) + (b % c)) % c)
+  console.log((a * b) % c)
+  console.log(((a % c) * (b % c)) % c)
+}
+
+solution(input)
