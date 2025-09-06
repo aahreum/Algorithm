dea17f1728b39317312c87b1cad16acb5fb8754c
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-06 12:10:30 +0900

    [Bronze III] Title: 알람 시계, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split(' ')
+
+function solution(input) {
+  const hour = Number(input[0])
+  const minute = Number(input[1])
+  let time = hour * 60 + minute - 45
+
+  if (time < 0) {
+    time += 24 * 60
+  }
+
+  const newHour = Math.abs(Math.floor(time / 60) % 24)
+  const newMinute = Math.abs(time % 60)
+
+  console.log(newHour, newMinute)
+}
+
+solution(input)
