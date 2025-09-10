6de7eb2db46eb24d0f011c3fc4e8809617cf2b24
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-10 20:40:17 +0900

    [Bronze V] Title: A+B - 3, Time: 100 ms, Memory: 9588 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n')
+
+function solution(input) {
+  for (let i = 1; i < input.length; i++) {
+    const [a, b] = input[i].split(' ')
+    console.log(Number(a) + Number(b))
+  }
+}
+
+solution(input)
