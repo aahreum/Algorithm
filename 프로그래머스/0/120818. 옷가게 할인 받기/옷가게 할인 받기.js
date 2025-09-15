8179beeba504af3be3ebdd04c5d0210c62b3a808
ahreum <117738875+aahreum@users.noreply.github.com>
2025-09-15 15:09:57 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,15 @@
+function solution(price) {
+    let discountRate = 0;
+    
+    if (price >= 500000) {
+        discountRate = 0.2;
+    } else if (price >= 300000) {
+        discountRate = 0.1;
+    } else if (price >= 100000) {
+        discountRate = 0.05
+    }
+    
+    const finalPrice = price * (1 - discountRate);
+
+    return Math.floor(finalPrice);
+}
