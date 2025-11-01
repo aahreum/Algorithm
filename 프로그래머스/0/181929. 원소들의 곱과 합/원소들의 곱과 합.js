fb8fc239e632264d1f97db1a693cfafa5243c596
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-01 17:21:34 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,5 +1,5 @@
 function solution(num_list) {
-    let mul = num_list.reduce((acc, curr) => {
+    const mul = num_list.reduce((acc, curr) => {
         return acc * curr
     }, 1);
     const sum = num_list.reduce((acc, curr) => {
