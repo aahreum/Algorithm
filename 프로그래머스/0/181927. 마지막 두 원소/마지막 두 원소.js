bf75d9453a074ec6c311921dcae117efe046e77a
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-05 16:13:05 +0900

    [level 0] Title: 마지막 두 원소, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.js
@@ -0,0 +1,6 @@
+function solution(num_list) {
+    const last = num_list[num_list.length - 1]
+    const prev = num_list[num_list.length - 2]
+    const answer = last > prev ? last - prev : last * 2;
+    return [...num_list, answer];
+}
