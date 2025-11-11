f0fe1ff0d09fe5425d5220275ca8d69f9a0aa4e8
ahreum <117738875+aahreum@users.noreply.github.com>
2025-11-11 17:07:16 +0900

    [level 0] Title: 문자열 섞기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.js
@@ -2,8 +2,7 @@ function solution(str1, str2) {
     let answer = '';
     
     for (let i = 0; i < str1.length; i++) {
-        answer += str1[i];
-        answer += str2[i];
+        answer += str1[i] + str2[i];
     }
     
     return answer;
