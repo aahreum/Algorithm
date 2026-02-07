db09a72c83a8732ce19482303b87b99f9014af0e
ahreum <cocoding420@gmail.com>
2026-02-07 16:04:51 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,17 +1,3 @@
 function solution(age) {
-    const stringAge = String(age);
-    const answer = [];
-    const alphabetArray = [];
-    const startIndex = 'a'.charCodeAt(0);
-    const lastIndex = 'z'.charCodeAt(0);
-
-    for (let i = startIndex; i <= lastIndex; i++) {
-      alphabetArray.push(String.fromCharCode(i));
-    }
-    
-    for (let i = 0; i < stringAge.length; i++) {
-        answer.push(alphabetArray[stringAge[i]]);
-    }
-    
-    return answer.join('');
+   return String(age).split('').map((s) => String.fromCharCode(97 + Number(s))).join('');
 }
