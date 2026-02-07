92af50c7932f25007a226364a9d63bbf84c78e76
ahreum <cocoding420@gmail.com>
2026-02-07 16:50:07 +0900

    [Silver IV] Title: 괄호, Time: 100 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Silver/9012. 괄호/괄호.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+
+function solution(input) {
+  const answer = [];
+
+  for (let i = 1; i < input.length; i++) {
+    const stack = [];
+
+    for (const ch of input[i]) {
+      if (ch === "(") {
+        stack.push(ch);
+      } else {
+        if (stack.length === 0) {
+          stack.push(")");
+          break;
+        }
+        stack.pop();
+      }
+    }
+
+    if (stack.length === 0) {
+      answer.push("YES");
+    } else {
+      answer.push("NO");
+    }
+  }
+
+  console.log(answer.join("\n"));
+}
+
+solution(input);
