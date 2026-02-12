4675eddb9441eedc1f3a09df4f1a823da410a922
ahreum <cocoding420@gmail.com>
2026-02-12 16:50:03 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -9,3 +9,14 @@ function solution(num_list, n) {
     
     return answer;
 }
+
+// slice 활용
+function solution(num_list, n) {
+    const answer = [];
+    
+    for (let i = 0; i < num_list.length; i += n) {
+        answer.push(num_list.slice(i, i + n));
+    }
+    
+    return answer;
+}
