83b667769aa6c87ed930b35b1d9213c49978a878
ahreum <cocoding420@gmail.com>
2026-02-13 17:44:06 +0900

    Optimize solution using Set for missing assignments
    Refactored solution to use a Set for better performance.

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -2,11 +2,10 @@ const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
 function solution(input) {
-  const array = input.sort((a, b) => a - b);
   const answer = [];
 
   for (let i = 1; i <= 30; i++) {
-    if (!array.includes(String(i))) {
+    if (!input.includes(String(i))) {
       answer.push(i);
     }
 
@@ -19,3 +18,21 @@ function solution(input) {
 }
 
 solution(input);
+
+// set으로 풀이
+function solution(input) {
+  const set = new Set(input);
+  const answer = [];
+
+  for (let i = 1; i <= 30; i++) {
+    if (!set.has(String(i))) {
+      answer.push(i);
+    }
+
+    if (answer.length === 2) {
+      break;
+    }
+  }
+
+  console.log(answer.join("\n"));
+}
