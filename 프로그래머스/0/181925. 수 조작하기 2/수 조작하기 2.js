62dd49d69b946013fb2be3230100465d3dc367ae
ahreum <cocoding420@gmail.com>
2026-03-03 20:06:22 +0900

    [level 0] Title: 수 조작하기 2, Time: 407.50 ms, Memory: 40.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -0,0 +1,18 @@
+function solution(numLog) {
+    let answer = '';
+    
+    for (let i = 0; i < numLog.length - 1; i++) {
+        const result = numLog[i + 1] - numLog[i];
+        if (result === 1) {
+            answer += 'w';
+        } else if (result === -1) {
+            answer += 's';
+        } else if (result === 10) {
+            answer += 'd';
+        } else {
+            answer += 'a';
+        }
+    }
+    
+    return answer;
+}
