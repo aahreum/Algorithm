cc19149180f44979d0648f94b3d22d7df37d79b3
ahreum <cocoding420@gmail.com>
2026-03-11 10:46:56 +0900

    [level 0] Title: 5명씩, Time: 0.11 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,11 @@
+function solution(names) {
+    const answer = [names[0]];
+    
+    for (let i = 1; i < names.length; i++) {
+        if (i % 5 === 0) {
+            answer.push(names[i]);
+        }
+    }
+    
+    return answer;
+}
