25fb760d99a7d15888ebeee5b9248c14586b5dbf
ahreum <cocoding420@gmail.com>
2026-03-11 11:35:13 +0900

    [level 0] Title: 빈 배열에 추가, 삭제하기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181860. 빈 배열에 추가， 삭제하기/빈 배열에 추가， 삭제하기.js
@@ -0,0 +1,15 @@
+function solution(arr, flag) {
+    const answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (flag[i]) {
+            for (let j = 0; j < arr[i] * 2; j++) {
+                answer.push(arr[i]);
+            }
+        } else {
+            answer.length -= arr[i];
+        }
+    }
+    
+    return answer;
+}
