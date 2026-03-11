7f1f202bca252090e50a5c7ae2dda06e67327083
ahreum <cocoding420@gmail.com>
2026-03-11 11:20:25 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 45.86 ms, Memory: 116 MB -BaekjoonHub

---
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    const answer = arr.map((num) => {
+        if (num >= 50 && num % 2 === 0) {
+            return num / 2;
+        } else if (num < 50 && num % 2 !== 0) {
+            return num * 2;
+        } else {
+            return num;
+        }
+    });
+    
+    return answer;
+}
