a228ff6453642c5a61a53212916a7c8a4f07d540
ahreum <cocoding420@gmail.com>
2026-03-12 15:58:14 +0900

    [level 0] Title: 글자 지우기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.js
@@ -1,6 +1,5 @@
 function solution(my_string, indices) {
     let answer = '';
-    indices.sort((a, b) => a - b);
     
     for (let i = 0; i < my_string.length; i++) {
         if (!indices.includes(i)) {
