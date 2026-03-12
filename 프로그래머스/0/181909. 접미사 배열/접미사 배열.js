ec541d351f2bb384ee0ce9e4c7b14ce51c1e4992
ahreum <cocoding420@gmail.com>
2026-03-12 16:05:56 +0900

    [level 0] Title: 접미사 배열, Time: 0.16 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    const answer = [];
+    
+    for (let i = my_string.length - 1; i >= 0; i--) {
+        answer.push(my_string.slice(i));
+    }
+    
+    return answer.sort();
+}
