d7732b0e874bf6a729cfb8ba06afd49afe7c1afc
ahreum <cocoding420@gmail.com>
2026-03-12 16:15:43 +0900

    [level 0] Title: 세로 읽기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.js
@@ -0,0 +1,15 @@
+function solution(my_string, m, c) {
+    if (m === 1) {
+        return my_string;
+    }
+    
+    let answer = '';
+    
+    for (let i = 0; i < my_string.length; i++) {
+        if (i % m === c - 1) {
+            answer += my_string[i];
+        }
+    }
+    
+    return answer;
+}
