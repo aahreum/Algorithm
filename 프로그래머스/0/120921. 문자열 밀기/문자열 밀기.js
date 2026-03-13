0921c8e2c5fbdd851203a03e307d9f342d1cfe8b
ahreum <cocoding420@gmail.com>
2026-03-13 10:39:02 +0900

    [level 0] Title: 문자열 밀기, Time: 0.21 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,11 @@
+function solution(A, B) {    
+    for (let i = 0; i < A.length; i++) {
+        if (A === B) {
+            return i;
+        }
+        
+        A = A.substring(A.length - 1) + A.substring(0, A.length - 1)
+    }
+    
+    return -1;
+}
