532bed8b491affb7607e6ea261562964d6b2d218
ahreum <cocoding420@gmail.com>
2026-03-18 11:12:29 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 100 ms, Memory: 9408 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+const [N, M] = input[0].split(" ").map(Number);
+
+function solution(n, m, queries) {
+  const array = Array.from({ length: n }, (_, i) => i + 1);
+
+  for (let idx = 0; idx < m; idx++) {
+    const [i, j] = queries[idx].split(" ").map((n) => Number(n) - 1);
+    const part = array.slice(i, j + 1).reverse();
+    array.splice(i, part.length, ...part);
+  }
+
+  return array.join(" ");
+}
+
+console.log(solution(N, M, input.slice(1)));
